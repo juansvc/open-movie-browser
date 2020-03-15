93ce1339a1ed569f08f118c14ccec10a15737a34
juansvc <juansebastianvc@gmail.com>
2020-03-15 02:10:14 -0500

    Create api.js - set axios baseURL for OMDB Api key

---
+++ src/api.js
@@ -0,0 +1,8 @@
+import axios from 'axios'
+
+// Set Free OMDB Api URL with key generated 
+// for juansebastianvc@gmail.com with axios
+axios.defaults.baseURL = 'http://www.omdbapi.com/?apikey=9ea9caf2&page=1&type=movie&r=json'
+
+export default {
+}
