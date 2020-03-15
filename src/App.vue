6baedcd784a2ad5a7c0725cbab71ad29f3fca0e6
juansvc <juansebastianvc@gmail.com>
2020-03-15 02:04:59 -0500

    Update App.vue - Change title, added toolbar, search component, data and method

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,26 @@
 <template>
   <v-app>
      <v-app-bar app>
-      Vue Js Task for Giffits Applicants in the Senior Frontend Developer position
+       <!-- Title toolbar -->
+      <v-toolbar-title class="headline text-uppercase">
+        <h3>Giffits - Eng. Juan Vivanco</h3>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-flex sm="3" md="2">
+        <!-- Search text field -->
+        <v-text-field
+          outlined
+          color="green"
+          dense
+          class="mt-6 mx-auto"
+          label="Type a Movie Name or Actor"
+          v-model="search"
+        ></v-text-field>
+      </v-flex>
+      <!-- Search button -->
+      <v-btn text color="green" large @click="search" @keyup.enter.native="search">
+        <v-icon>mdi-magnify</v-icon>Search
+      </v-btn>
     </v-app-bar>
 
     <v-content>
@@ -17,5 +36,17 @@ export default {
   components: {
   },
 
+  // String to send for search
+  data() {
+    return {
+      search: ""
+    };
+  },
+  // Methon for search movies
+  methods: {
+    search() {
+      this.search = "";
+    }
+  }
 };
 </script>
