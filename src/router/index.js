26ba67082cf0373d1009e7f4778aca2dbc64f094
juansvc <juansebastianvc@gmail.com>
2020-03-15 02:17:08 -0500

    Update vue-route with Movies.vue component

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,15 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Movies from '../components/Movies.vue'
 
 Vue.use(VueRouter)
-
+//Routes to components
 const routes = [
-  
+  {
+    path: '/',
+    name: 'Movies',
+    component: Movies
+  },
 ]
 
 const router = new VueRouter({
