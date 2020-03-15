8980911d7ce5f7fd267411e4f2261493298f078f
juansvc <juansebastianvc@gmail.com>
2020-03-15 02:25:43 -0500

    Fetch from api iron man movies, added axios in package.json dependencies

--- src/components/Movies.vue
+++ src/components/Movies.vue
@@ -10,11 +10,24 @@
 </template>
 
 <script>
+import api from "../api";
 
 export default {
   data () {
     return {
     }
+  },
+//   Fetch from api.js for first view movie cards on component mounted
+  mounted() {
+    api
+      .fetchmovies("iron man")
+      .then(res => {
+        this.movies = res.Search;
+      })
+      .catch(error => {
+        console.log(error);
+      });
+
   },
   methods: {
   }
