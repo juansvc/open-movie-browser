7e96d078b467feeb3f71c46d113e5a5652765833
juansvc <juansebastianvc@gmail.com>
2020-03-15 13:43:56 -0500

    Update InfoMovie.vue component -  Create card and button for ratings, added map function to retreive rattings from sources in api

--- src/components/InfoMovie.vue
+++ src/components/InfoMovie.vue
@@ -35,8 +35,46 @@
     <v-layout row wrap>
       <v-flex mt-n12 mx-auto>
         <div class="text-center">
+            <!-- Dialog for movie ratings -->
+          <v-dialog v-model="dialog" width="500">
           <!-- Button for ratings -->
-          <v-btn large color="green" dark>View Ratings</v-btn>
+        <template v-slot:activator="{ on }">
+          <v-btn slot="activator" large color="green" v-on="on" dark>View Ratings</v-btn>
+        </template>
+        <v-card>
+            <!-- Card for ratings -->
+              <v-card-title class="headline grey lighten-2">Ratings</v-card-title>
+              <v-card-text>
+                <table>
+                  <thead></thead>
+                  <tbody>
+                    <tr v-for="(rating, index) in this.ratings" :key="index">
+                      <td align="center">{{ ratings[index].Source }}</td>
+                      <td align="center">
+                          <!-- Ratings -->
+                        <v-rating
+                          :half-increments="true"
+                          :value="ratings[index].Value"
+                          :empty-icon="emptyIcon"
+                          :full-icon="fullIcon"
+                          :half-icon="halfIcon"
+                          :color="color"
+                          :background-color="bgColor"
+                          :readonly="readonly"
+                        ></v-rating>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </v-card-text>
+              <v-divider></v-divider>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <!-- Close ratings dialog button -->
+                <v-btn outlined color="green" x-large text @click="dialog = false">EXIT</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
         </div>
       </v-flex>
     </v-layout>
@@ -52,7 +90,15 @@ export default {
   data() {
     return {
       movie: "",
-      loading: true
+      loading: true,
+      dialog: false,
+      ratings: "",
+      emptyIcon: "mdi-heart-outline",
+      fullIcon: "mdi-heart",
+      halfIcon: "mdi-heart-half-full",
+      color: "red lighten-3",
+      readonly: true,
+      bgColor: "grey lighten-1"
     };
   },
   //   Fetch from api.js for movie selected on component mounted
@@ -61,6 +107,14 @@ export default {
       .fetchmovie(this.id)
       .then(res => {
         this.movie = res;
+        this.ratings = this.movie.Ratings;
+        // Map for get ratings in float from sources sent by api
+        // (Internet Movie Database, Rotten Tomatoes, Metacritic)
+        this.ratings.map(function(rat) {
+          rat.Value = parseFloat(rat.Value.split(/\/|%/)[0]);
+          rat.Value = rat.Value <= 10 ? rat.Value / 2 : rat.Value / 20;
+        });
+        this.loading
         this.loading = false;
       })
       .catch(error => {
