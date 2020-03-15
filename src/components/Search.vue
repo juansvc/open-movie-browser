0136bff54ff5b3e0a1778d5f53de10cc4776317c
juansvc <juansebastianvc@gmail.com>
2020-03-15 13:54:07 -0500

    Update Search.vue component - Added data iteraror, toolbar, list view and keys for sorting movies by year, title, and imbdapi

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -14,8 +14,36 @@
 <!-- container when data ready -->
   <v-container v-else grid-list-xl>
     <v-container fluid>
+        <!-- data iterator for sorting movies by year, title, and imbdapi -->
+        <v-data-iterator :items="items" :sort-by="sortBy" :sort-desc="sortDesc" hide-default-footer>
+        <template v-slot:header>
+          <v-toolbar dark color="green darken-3" class="mb-1">
+            <template v-if="$vuetify.breakpoint.mdAndUp">
+              <v-spacer></v-spacer>
+              <v-select
+                v-model="sortBy"
+                flat
+                solo-inverted
+                hide-details
+                :items="keys"
+                label="Sort by"
+              ></v-select>
+              <v-spacer></v-spacer>
+              <v-btn-toggle v-model="sortDesc" mandatory>
+                <v-btn large depressed color="green" :value="false">
+                  <v-icon>mdi-arrow-up</v-icon>
+                </v-btn>
+                <v-btn large depressed color="green" :value="true">
+                  <v-icon>mdi-arrow-down</v-icon>
+                </v-btn>
+              </v-btn-toggle>
+            </template>
+          </v-toolbar>
+        </template>
+
+        <template v-slot:default="props">
           <v-row>
-            <v-col v-for="item in items" :key="item.Title" cols="12" sm="1" md="3" lg="4">
+            <v-col v-for="item in props.items" :key="item.Title" cols="12" sm="1" md="3" lg="4">
               <!-- Movie cards -->
               <v-card>
                 <v-img :src="item.Poster" aspect-ratio="1.5"></v-img>
@@ -25,18 +53,31 @@
                   item.Title
                   }}
                 </v-card-title>
-                <v-card-text>
-                    <div class="body-2">Year: {{item.Year}}</div>
-                    <div class="body-2">Type: {{item.Type}}</div>
-                    <div class="body-2">IMDB-id: {{item.imdbID}}</div>
-                </v-card-text>
                 <v-divider></v-divider>
+                <!-- Order list by selected option year, title, or imbdapi -->
+                <v-list dense>
+                <v-card-text>
+                    <div v-for="(key, index) in filteredKeys" :key="index">
+                      <span :class="{ 'green--text': sortBy === key }">{{ key }}:</span>
+                      <span
+                        class="align-end"
+                        :class="{ 'green--text': sortBy === key }"
+                      >{{ item[key] }}</span>
+                    </div>
+                    <div class="body-2">Type: {{ item.Type }}</div>
+                </v-card-text>
+                </v-list>
                 <v-card-actions class="justify-center">
+                    <!-- View movie info button -->
                   <v-btn text outlined color="green" x-large @click="movie(item.imdbID)">View</v-btn>
                 </v-card-actions>
               </v-card>
             </v-col>
           </v-row>
+        </template>
+
+        <template v-slot:footer></template>
+        </v-data-iterator>
     </v-container>
   </v-container>
 </template>
@@ -50,10 +91,19 @@ export default {
     return {
       items: [],
       loading: true,
-      data: false
+      data: false,
+      filter: {},
+      sortDesc: false,
+      sortBy: "Title",
+      keys: ["Title", "Year", "imdbID"]
     };
   },
-
+  //Filter or sort at component mounted by Movie titles
+  computed: {
+    filteredKeys() {
+      return this.keys.filter(key => key !== `Title`);
+    }
+  },
   methods: {
     movie(id) {
       this.$router.push("/movie/" + id);
