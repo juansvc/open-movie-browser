3f1c8bc2f58f7bc185e3b06d800c01e6437e47c3
juansvc <juansebastianvc@gmail.com>
2020-03-15 02:14:42 -0500

    Create Movies.vue component

---
+++ src/components/Movies.vue
@@ -0,0 +1,22 @@
+<template>
+
+<!-- Container and layout for movie cards -->
+  <v-container grid-list-xl>
+    <v-layout class="d-flex flex-wrap">
+
+    </v-layout>
+  </v-container>
+
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+    }
+  },
+  methods: {
+  }
+}
+</script>
