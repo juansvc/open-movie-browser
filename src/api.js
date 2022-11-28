a0551d344c021d397ba96fed695cb3b12b0f5f7b
juansvc <juansebastianvc@gmail.com>
2022-11-27 21:17:33 -0500

    Update api.js

--- src/api.js
+++ src/api.js
@@ -10,7 +10,7 @@ import axios from 'axios'
 
 // Set Free OMDB Api URL with key generated 
 // for juansebastianvc@gmail.com with axios
-axios.defaults.baseURL = 'http://www.omdbapi.com/?apikey=9ea9caf2&page=1&type=movie&r=json'
+axios.defaults.baseURL = 'https://www.omdbapi.com/?apikey=9ea9caf2&page=1&type=movie&r=json'
 
 export default {
     // Method for fetch first view and searched movies
